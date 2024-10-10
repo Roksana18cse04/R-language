36f64b7a70a6372d6b29b6200dad6d0a9b51b6a8
Roksana Akter <roksana18cse04@gmail.com>
2024-10-10 18:42:17 +0600

    message

---
+++ array.R
@@ -0,0 +1,14 @@
+# Array Dimention 2,4,3
+
+arr<- array(1:8)
+print(arr)
+print(arr[4])
+
+# Creating two dimetional array
+
+arr1=array(1:6,dim=c(2,3)) # here 2=dimentional ,col=3
+print(arr1)
+
+# crating three dimentional array
+arr2=array(1:24,dim=c(2,3,4))
+print(arr2)
